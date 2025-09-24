a5fa39f97fa27ed9ae48a260b98c949ab0dab9c4
Sarah <peasblinky@gmail.com>
2025-09-24 15:03:51 +0200

    updated the domain layer

---
+++ Domain/Booking/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Booking
+{
+    public class Booking
+    {
+        public int Id { get; init; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string ApprovalStatus { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public Booking(DateTime startTime, DateTime endTime, string approvalStatus, DateTime creationDate)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            ApprovalStatus = approvalStatus;
+            CreationDate = creationDate;
+        }
+    }
+}
