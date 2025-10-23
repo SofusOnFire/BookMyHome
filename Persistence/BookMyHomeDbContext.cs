0a840a7442688f13ef6e51f7cbd055fede4d2d1f
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-10-23 12:35:08 +0200

    Push

--- Persistence/BookMyHomeDbContext.cs
+++ Persistence/BookMyHomeDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,27 @@ using System.Threading.Tasks;
 
 namespace Persistence
 {
-    public class BookMyHomeDbContext
+    public class BookMyHomeDbContext : DbContext
     {
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+
+        /// <summary>
+        /// Gets Connectionstring from local folder on Desktop
+        /// </summary>
+        /// <returns></returns>
+        protected static string GetConnectionString()
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fullName = Path.Combine(desktopPath, "RecipeAppConnectionString.txt");
+            return File.ReadAllText(fullName);
+        }
     }
 }
